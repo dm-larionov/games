efaf19feda9a3140c6941cbabfe2feb707654245
dm-larionov <112629282+dm-larionov@users.noreply.github.com>
2023-10-04 13:15:21 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            int[] nums2 = new int[34];
+
+            for (int i = 0; i < 222; i++)
+            {
+                nums2[i] = i;
+            }
+        }
+    }
+}
